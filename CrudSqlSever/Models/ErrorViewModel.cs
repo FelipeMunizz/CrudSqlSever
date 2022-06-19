7bb6474a7b643dfc40da292a2355e3db3c35aa1d
Felipe Muniz <lipe.baterra@gmail.com>
2022-06-19 19:02:26 -0300

    Adicionar arquivos de projeto.

---
+++ CrudSqlSever/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudSqlSever.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
